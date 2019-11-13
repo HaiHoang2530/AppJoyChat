68d547e0d34330bfaabee7da728ec1144801363e
Hoang Quang Hai hai hoang <haihqph05936@fpt.edu.vn>
2019-11-13 07:47:48 +0700

    Initial commit

---
+++ app/src/main/java/haihqph05936/iris/appjoychat/Login_Joy.kt
@@ -0,0 +1,12 @@
+package haihqph05936.iris.appjoychat
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Login_Joy : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login__joy)
+    }
+}
